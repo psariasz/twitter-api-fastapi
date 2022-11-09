8b2d9787df604299e34af2d4ec487234d4ee6498
Pablo Arias <psariasz@gmail.com>
2022-11-09 12:31:36 -0500

    models_users

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+#Pydantic
+from pydantic import BaseModel
+
+#FastAPI
 from fastapi import FastAPI
 
 app= FastAPI()
+
+# Models
+
+class User(BaseModel):
+    pass
+
+class Tweet(BaseModel):
+    pass
+
 
 @app.get(path="/")
 def home():
